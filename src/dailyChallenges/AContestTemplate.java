6ae59adbef4f98f90a136d9e194ed38d4110fc8f
Albir Tarsha <a.tarsha@yahoo.com>
2021-01-09 11:38:49 -0600

    AContestTemplate

---
+++ src/dailyChallenges/AContestTemplate.java
@@ -0,0 +1,13 @@
+package dailyChallenges;
+
+class SolutionWC123456789 {
+    public int prob(int num) {
+        return num;
+    }
+}
+public class AContestTemplate {
+    public static void main(String[] args) {
+        SolutionWC123456789 solution = new SolutionWC123456789();
+        System.out.println(solution.prob(4));
+    }
+}
