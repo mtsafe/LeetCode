2f0a5e36a1be0c47d149e8baf1c2d005726dc257
Albir Tarsha <a.tarsha@yahoo.com>
2021-01-31 20:51:03 -0600

    added ListNode.java for Feb

---
+++ src/dayChallFeb2021/ListNode.java
@@ -0,0 +1,19 @@
+package dayChallFeb2021;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class ListNode {
+    int val;
+    contests.ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, contests.ListNode next) { this.val = val; this.next = next; }
+}
