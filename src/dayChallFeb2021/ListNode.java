325811991891ae300f4773f8c796b8530dfc45af
Albir Tarsha <a.tarsha@yahoo.com>
2021-02-03 13:40:06 -0600

    Day103_2021

--- src/dayChallFeb2021/ListNode.java
+++ src/dayChallFeb2021/ListNode.java
@@ -12,8 +12,8 @@ package dayChallFeb2021;
  */
 public class ListNode {
     int val;
-    contests.ListNode next;
+    ListNode next;
     ListNode() {}
     ListNode(int val) { this.val = val; }
-    ListNode(int val, contests.ListNode next) { this.val = val; this.next = next; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
